559dc1997da7b6046297a6eb0d9b20a7524b2ff4
Sylvan <sylvan700@hotmail.fr>
2019-05-14 17:39:15 +0200

    v0.1

---
+++ Room.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    public class Room
+    {
+        public int ID;
+        public string description;
+        public Room(int myID, string myDescription)
+        {
+            ID = myID;
+            description = myDescription;
+        }
+
+        public Dictionary<string, int> myExit = new Dictionary<string, int>(); 
+        
+    }
+}
--- excercices.cs
+++ excercices.cs
@@ -14,9 +14,6 @@ namespace ConsoleApp5
 
             Game myGame = new Game();
             myGame.Main();
-
-
-
         }
     }
 }
